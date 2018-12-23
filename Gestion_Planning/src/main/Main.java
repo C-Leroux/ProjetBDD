f43c428631edbe8e93265992386aafa42bc51943
camille <mouttecam@gmail.com>
2018-12-23 16:12:32 +0100

    add teste

--- Gestion_Planning/src/main/Main.java
+++ Gestion_Planning/src/main/Main.java
@@ -59,7 +59,7 @@ public class Main {
 			Salle salle4 = Salle.creerSalle("E04", 30, RoomType.COURS);
 			
 			
-			// Creation d'un cours
+			// Creation du cours 1
 			Calendar calendar = Calendar.getInstance();
 			calendar.set(2018, 12, 24, 12, 0);
 			Date date = calendar.getTime();
@@ -72,7 +72,23 @@ public class Main {
 			 
 			java.sql.Date lundi4 = new java.sql.Date(date2.getTime());
 			
-			Cours cours = Cours.creerCours("algo", lundi2, lundi4, 1L , 1L, 1L, 1);
+			Cours cours = Cours.creerCours("algorithmique", lundi2, lundi4, 1L , 1L, 1L, 1);
+			
+			
+			// Creation du cours 2
+
+			Calendar calendar1 = Calendar.getInstance();
+			calendar1.set(2018, 12, 24, 12, 0);
+			Date date1 = calendar1.getTime();
+			 
+			java.sql.Date lundi3 = new java.sql.Date(date1.getTime());
+			 
+			Calendar calendar3 = Calendar.getInstance();
+			calendar3.set(2018, 12, 24, 14, 0);
+			Date date3 = calendar3.getTime();
+			 
+			java.sql.Date lundi5 = new java.sql.Date(date3.getTime());
+			Cours cours1 = Cours.creerCours("theorie des grpahe", lundi3, lundi5, 1L , 1L, 1L, 1);
 			
 			// creation administrateur
 			Administrateur admin = Administrateur.creerAdministrateur("admin", "admin", "admin", "0698844194",  "0698844194", "Adminadmin", "admin@gmail.fr", "99", "94270", "kremlin bicetre");
@@ -86,22 +102,65 @@ public class Main {
 			
 			//Cours.supprimerCours(2L);
 			System.out.println("------------------");
+			System.out.println("-- Liste des Cours --");
+			System.out.println("------------------");
 		    ArrayList<Cours> listcours = eleve.getCoursByIdSemaine(1);
-		    System.out.println(listcours.size());
 		    for(int i = 0; i < listcours.size(); i++) {
-		    	System.out.println(listcours.get(i).getNom());
+		    	System.out.println(listcours.get(i).getNom() + " " + listcours.get(i).getDateDebut());
 		    }
+		    System.out.println("------------------");   
+		    System.out.println("-- Liste des utilisateurs --");  
 		    System.out.println("------------------");
-		      
-		      
 			DbConnexion db = new DbConnexion("SELECT * FROM utilisateur;");
 			ResultSet rs = db.executerRequete();
 			while (rs.next()) {
 			    long matricule = rs.getLong("matricule");
 			    String nom = rs.getString("nom");
 			    String prenom = rs.getString("prenom");
-			    System.out.println(matricule + " " + nom + " " + prenom);
+			    String role = rs.getString("role");
+			    System.out.println(matricule + " " + nom + " " + prenom + " " + role);
 			}
+			
+			System.out.println("------------------");   
+		    System.out.println("-- Liste des salles --");  
+		    System.out.println("------------------");
+			DbConnexion db1 = new DbConnexion("SELECT * FROM SALLE;");
+			ResultSet rs1 = db1.executerRequete();
+			while (rs1.next()) {
+			    String nom = rs1.getString("nom");
+			    int nbPlaces = rs1.getInt("nbPlaces");
+			    String typeSalle = rs1.getString("typeSalle");
+			    System.out.println(nom +  " " + nbPlaces + " " + typeSalle);
+			}
+			
+			
+			System.out.println("------------------");   
+		    System.out.println("-- Liste des Responsables eleves --");  
+		    System.out.println("------------------");
+			DbConnexion db2 = new DbConnexion("SELECT * FROM RESPONSABLE;");
+			ResultSet rs2 = db2.executerRequete();
+			while (rs2.next()) {
+			    String nom = rs2.getString("nomResp");
+			    String prenom = rs2.getString("prenomResp");
+			    int matricule = rs2.getInt("matricule");
+			    System.out.println(nom + " " + prenom + "  Responbla de l'eleve " + matricule);
+			}
+			
+			
+			System.out.println("------------------");   
+		    System.out.println("-- Liste des Groupes --");  
+		    System.out.println("------------------");
+			DbConnexion db3 = new DbConnexion("SELECT * FROM GROUPE;");
+			ResultSet rs3 = db3.executerRequete();
+			while (rs3.next()) {
+			    String nom = rs3.getString("nom");
+			    String promotion = rs3.getString("promotion");
+			    System.out.println(nom + " " + promotion );
+			}
+			
+			
+			
+			// fermeture de la base de donnée
 			db.fermerConnexion();
 			
 		} catch (ClassNotFoundException e) {
